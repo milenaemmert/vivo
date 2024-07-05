a51727315d9495267ba4d0ef235fa49965abc2e6
milenaemmert <milena.emmert@outlook.com>
2024-07-05 11:46:45 -0300

    Explicação do hook personalizado useScroll

--- src/hooks/use-scroll.js
+++ src/hooks/use-scroll.js
@@ -24,3 +24,8 @@ export const useScroll = () => {
 
   return { pageY, isPageTop }
 }
+
+//pageY => ponto 0px do meu eixo Y
+//o useEffect vai ser executado apenas quando o componente se montar, porque meu array de dependência de está vazio
+//a função handlePageY vai ser a lógica para verificar com meu addEventListener se meu
+//o removeEventListener serve para quando meu componente se desmontar, o addEventListener pare de ser escutado e pare de consumir memória sem necessidade
